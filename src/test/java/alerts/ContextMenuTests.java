4cdaf585a523e28d9471078a438194a395f88962
Sebastian Puerta <133784886+sebaspuerta95@users.noreply.github.com>
2024-11-19 14:46:10 -0400

    Update ContextMenuTests.java
    Included JavaDoc and relation to ContextMenuPage class.

--- src/test/java/alerts/ContextMenuTests.java
+++ src/test/java/alerts/ContextMenuTests.java
@@ -5,8 +5,20 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
+/**
+* This test class is an example of usage of the Actions class to send right-clicks to element and interact with JavaScript alerts. 
+* It is related to {@link ContextMenuPage }
+*/ 
 public class ContextMenuTests extends BaseTestsTAU {
 
+    /**
+    * Process:
+    * <ol>
+    *   <li>A right-click is sent to the content box.</li>
+    *   <li>A JavaScript alert is triggered and the test switches to the alert to grab the text.</li>
+    *   <li>The test accepts the alert and validates that the text matches the expected result.</li>
+    * </ol>    
+    */ 
     @Test
     public void testContextBoxAlert(){
         var contextMenuPage = homePage.clickContextMenu();
