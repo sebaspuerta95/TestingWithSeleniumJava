28ee4aaee4473022fbd90ad194bd7cfe04d84291
Sebastian Puerta <133784886+sebaspuerta95@users.noreply.github.com>
2024-11-19 16:36:25 -0400

    Update FileUploaderPage.java
    Included Java Doc.

--- src/main/java/pages/FileUploaderPage.java
+++ src/main/java/pages/FileUploaderPage.java
@@ -3,6 +3,9 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+/**
+* This class models the file uploader page that is tested in the { @FileUploaderTests } test class (src/test/alerts). 
+*/ 
 public class FileUploaderPage {
 
     private WebDriver driver;
@@ -18,6 +21,7 @@ public class FileUploaderPage {
         driver.findElement(uploadButton).click();
     }
 
+    // A file's absolute path is required to be sent as keys to upload a file to the browser. It is easier than handling the file upload window.
     public void uploadFile(String absolutePathOfFile){
         driver.findElement(chooseFileButton).sendKeys(absolutePathOfFile);
         clickUploadButton();
