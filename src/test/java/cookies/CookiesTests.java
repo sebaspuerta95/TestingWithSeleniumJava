dd795932d6fd724a377e7feb191a772e845eee57
Sebastian Puerta <133784886+sebaspuerta95@users.noreply.github.com>
2024-11-19 16:07:39 -0400

    Update CookiesTests.java
    Included JavaDoc.

--- src/test/java/cookies/CookiesTests.java
+++ src/test/java/cookies/CookiesTests.java
@@ -5,8 +5,22 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertTrue;
 
+/**
+* This test class is an example of how we can add, read or remove cookies to process data. 
+* Cookies use the Builder pattern to help create cookies more effitiently. 
+* It related to { @CookieManager } class. 
+*/ 
 public class CookiesTests extends BaseTestsTAU {
 
+    /**
+    * Process:
+    * <ol>
+    *   <li>Cookie name and value are defined in the test.</li>
+    *   <li>Variables are passed to the buildCookie() method and the cookie is stored.</li>
+    *   <li>A breakpoint can be added here to ensure that the cookie is stored properly before deleting it.</li>
+    *   <li>The cookie manager deletes the cookie again and it is validated that the cookie is deleted properly.</li>
+    * </ol>    
+    */
     @Test
     public void testDeleteCookie(){
         String cookieName = "optimizelyBuckets";
