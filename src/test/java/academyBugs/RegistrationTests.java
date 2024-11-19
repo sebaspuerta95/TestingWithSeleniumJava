f25661371c329ce01ae07965516e1e8d8e85ed4e
Sebastian Puerta <133784886+sebaspuerta95@users.noreply.github.com>
2024-11-19 13:50:26 -0400

    Update RegistrationTests.java
    Included groups attribute.

--- src/test/java/academyBugs/RegistrationTests.java
+++ src/test/java/academyBugs/RegistrationTests.java
@@ -6,12 +6,16 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import java.nio.file.Paths;
 
+/**
+* This class was created to demonstrate how to use data-driven testing while submitting a registration form. 
+* It is integrated with TestNG attributes to control de sources of information. 
+*/ 
 public class RegistrationTests extends BaseAB {
 
     // This class is an example of usage of soft assertions, aka validation testing.
 
     // Example importing the test data from a CSV file.
-    @Test (priority = 1, dataProvider = "csvDataProvider", dataProviderClass = SupplierClass.class)
+    @Test (priority = 1, dataProvider = "csvDataProvider", dataProviderClass = SupplierClass.class, groups = "academyBugs")
     public void testRegistrationWithCSV (String firstname, String lastname, String email, String retypeEmail,
                                   String password, String retypePassword,
                                   String expectedFirstnameMessage, String expectedLastnameMessage,
@@ -33,7 +37,7 @@ public class RegistrationTests extends BaseAB {
     }
 
     // Example using the test data from the IDE.
-    @Test (priority = 2, dataProvider = "testDataSupplier", dataProviderClass = SupplierClass.class)
+    @Test (priority = 2, dataProvider = "testDataSupplier", dataProviderClass = SupplierClass.class, groups = "academyBugs")
     public void testRegistrationWithTestDataInArray (String firstname, String lastname, String email, String retypeEmail,
                                          String password, String retypePassword,
                                          String expectedFirstnameMessage, String expectedLastnameMessage,
