7cf6b677e5703f23d5351a90f3e5fe73dd86dc69
Sebastian Puerta <133784886+sebaspuerta95@users.noreply.github.com>
2024-11-19 14:11:21 -0400

    Update HoversTests.java
    Included JavaDoc and dependency attributes to the test annotation.

--- src/test/java/hovers/HoversTests.java
+++ src/test/java/hovers/HoversTests.java
@@ -8,9 +8,19 @@ import static org.testng.Assert.*;
 /**
 * This test class was created to demonstrate the usage of the Actions class to hover over elements and interact with them. 
 * This test class is related to {@link HoversPage} class that models the page with its fields and methods.
+* It also uses dependency testing to showcase the usage of TestNG.
 */ 
 public class HoversTests extends BaseTestsTAU {
 
+    /**
+    * Each of the following 3 test methods creates an instance of the HoversPage to validate the information that is displayed after hovering over an image. 
+    * Process:
+    * <ol>
+    *   <li>Hover over a figure caption.</li>
+    *   <li>Validate that the figure is displayed after we hover over it.</li>
+    *   <li>Validate that the information that is displayed is the expected information.</li>
+    * </ol>
+    */ 
     @Test (groups = "hoverTests")
     public void testHoverUser1(){
         var hoversPage = homePage.clickHovers();
