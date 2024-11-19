27ef640656df7eb248fb237677c011fe3fd0b636
Sebastian Puerta <133784886+sebaspuerta95@users.noreply.github.com>
2024-11-19 17:50:07 -0400

    Update DropdownTests.java
    Included JavaDoc.

--- src/test/java/dropdown/DropdownTests.java
+++ src/test/java/dropdown/DropdownTests.java
@@ -8,8 +8,13 @@ import java.util.List;
 
 import static org.testng.Assert.*;
 
+/**
+* This test class tests the usage of dropdown elements. It requires the Selenium Support dependency from Maven to be included to the pom.xml file. 
+* It is related to {@link DropdownPage } at src/main/pages
+*/ 
 public class DropdownTests extends BaseTestsTAU {
 
+    // Tests that only 1 option was selected and that it contains the expected text.
     @Test
     public void testSelectedOption(){
         var dropdownPage = homePage.clickDropdownMenu();
@@ -20,11 +25,13 @@ public class DropdownTests extends BaseTestsTAU {
         assertTrue(selectedOptions1.contains(option), "Option not selected");
     }
 
+    // Changes the <select> HTML tag to allow multiple options and tests that all the options were added.
     @Test
     public void testSelectMultipleOptions(){
         var multipleDropdown = homePage.clickDropdownMenu();
         multipleDropdown.setMultipleAttribute();
 
+        // A list with all the options is created, and then they are added to the options.
         List<String> optionsSelected = Arrays.asList("Option 1", "Option 2");
         for (String option : optionsSelected){
             multipleDropdown.selectFromDropdown(option);
