1284b8a40f043c8e7dd84c7e079511805385643e
Sebastian Puerta <133784886+sebaspuerta95@users.noreply.github.com>
2024-11-19 20:46:10 -0400

    Update ForgotPasswordTests.java

--- src/test/java/forgotPassword/ForgotPasswordTests.java
+++ src/test/java/forgotPassword/ForgotPasswordTests.java
@@ -3,10 +3,18 @@ package forgotPassword;
 import base.BaseTestsTAU;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
+import pages.ForgotPasswordPage;
+import pages.EmailSentPage;
 
+/**
+* This test class contains a simple test that consists in sending a password recovery request and grabbing a message from the page. 
+* It is related to {@link ForgotPasswordPage } and {@link EmailSentPage } at src/main/pages
+*
+* This test will always fail because the EmalSentPage is curently broken at the server.
+*/
 public class ForgotPasswordTests extends BaseTestsTAU {
 
-    // This test will always fail because the EmalSentPage is broken.
+    // 
     @Test
     public void testRetrievePassword(){
         var forgotPass = homePage.clickForgotPassword();
