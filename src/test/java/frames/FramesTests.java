cca824e0f18516d5006cfcf15d2b6ee7ef7c082a
Sebastian Puerta <133784886+sebaspuerta95@users.noreply.github.com>
2024-11-19 21:03:57 -0400

    Update FramesTests.java
    Included JavaDoc

--- src/test/java/frames/FramesTests.java
+++ src/test/java/frames/FramesTests.java
@@ -6,8 +6,20 @@ import pages.HomePage;
 
 import static org.testng.Assert.assertEquals;
 
+/**
+* This test class demonstrates how to interact with iFrames, which are <html> tags inside other <html> tags, and we cannot easily locate web elements inside of it. 
+* It is related to {@link WYSIWYGEditorPage } and {@link NestedFramesPageat } at src/main/pages
+*/
 public class FramesTests extends BaseTestsTAU {
 
+    /** 
+    * This method tests the interactions with a WYSIWYG edit (What You See Is What You Get).
+    * Process: 
+    * <ol>
+    * <li>The test accesses the iFrame to clear the text area and send new keys.</li>
+    * <li>The test interacts with the WYSIWIG editor and ensures that chagnes are reflected in the text area.</li>
+    * </ol>
+    */
     @Test
     public void testIncreaseIndentation(){
         var editorPage = homePage.clickWYSIWYGEditor();
@@ -20,6 +32,16 @@ public class FramesTests extends BaseTestsTAU {
         assertEquals(editorPage.getFrameText(), text1+text2, "Incorrect frame text.");
     }
 
+    /** 
+    * This method tests the interactions with a nested frames.
+    * Process: 
+    * <ol>
+    * <li>To get the text, the test accesses top <html> frame inside the global <html>, and then accesses left <html> frame inside top <html> frame.</li>
+    * <li>Similar steps to get the bottom <html> frame text. </li>
+    * </ol>
+    * 
+    * It is important to enter and exit each frame to ensure test consistency and prevent errors. 
+    */
     @Test
     public void testLeftAndBottomFrames(){
         var nestedFrames = homePage.clickNestedFrames();
