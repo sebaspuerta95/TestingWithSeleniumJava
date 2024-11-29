5f31134d74d9cd820b52052fd6ebac54bafc41e3
Sebastian Puerta <133784886+sebaspuerta95@users.noreply.github.com>
2024-11-29 19:27:25 -0400

    Update HoversPage.java
    Javadoc included.

--- src/main/java/pages/HoversPage.java
+++ src/main/java/pages/HoversPage.java
@@ -5,6 +5,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
+/**
+* This class models the context menu page that is tested in the HoversTests test class (src/test/hovers). 
+* It uses the Actions class to hover over a web element.
+*/ 
 public class HoversPage {
 
     private WebDriver driver;
@@ -16,8 +20,10 @@ public class HoversPage {
     }
 
     /**
-     * @param index Starts at 1.
-     */
+    * This methods is used to hover over the specified web element in a list. 
+    * @param index Starts at 1 and it represents the image in which we want to hover over.
+    * @return FigureCaption : Returns a handle to the internal class FigureCaption that models the caption that is displayed when we hover over an element. 
+    */
     public FigureCaption hoverOverImage(int index) {
         WebElement selectedElement = driver.findElements(figureBox).get(index-1);
         Actions actions = new Actions(driver);
@@ -25,6 +31,7 @@ public class HoversPage {
         return new FigureCaption(selectedElement.findElement(figureCaption));
     }
 
+    // Since hovering over an element does not take us to another page in this example, but displays a caption, we create an inner class to model it. 
     public class FigureCaption {
 
         private WebElement caption;
@@ -35,18 +42,30 @@ public class HoversPage {
             this.caption = caption;
         }
 
+        /**
+        * @return boolean : Returns true if the caption is displayed and false if it is not. 
+        */
         public boolean isCaptionDisplayed(){
             return caption.isDisplayed();
         }
 
+        /**
+        * @return String : Returns the title of the caption.
+        */
         public String getTitle(){
             return caption.findElement(header).getText();
         }
 
+        /**
+        * @return String : Returns the link of the caption.
+        */
         public String getLink(){
             return caption.findElement(link).getAttribute("href");
         }
 
+        /**
+        * @return String : Returns the text of the caption.
+        */
         public String getLinkText(){
             return caption.findElement(link).getText();
         }
