263ce2f0889c7a92ecda55ed1ef0b4f7dc76b126
Sebastian Puerta <133784886+sebaspuerta95@users.noreply.github.com>
2024-11-29 19:14:57 -0400

    Update SliderTests.java
    Added Javadoc.

--- src/test/java/horizontalSlider/SliderTests.java
+++ src/test/java/horizontalSlider/SliderTests.java
@@ -5,8 +5,21 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
+/**
+ * This test class demonstrates how to interact with a slider to move it until the desired point. 
+ * It is related to {@link pages.HorizontalSliderPage} at src/main/pages
+ */
 public class SliderTests extends BaseTestsTAU {
 
+    /**
+    * In this test, we move the slider until the desired element. It is helpful when we want to test the behavior at a certain point. 
+    * Process:
+    * <ol>
+    *   <li>Locate the horizontal slider and click on it to select it.</li>
+    *   <li>Move the slider by sending a right click until it matches the desired value.</li>
+    *   <li>Keys are sent while the message doesn't match the desired value.</li>
+    * </ol>    
+    */ 
     @Test
     public void testRightKey(){
         var horizontalSlidePage = homePage.clicHorizontalSlider();
