c31c0be283dec3941710744e12ca816a4fb47ecb
Sebastian Puerta <133784886+sebaspuerta95@users.noreply.github.com>
2024-11-29 19:15:00 -0400

    Update HorizontalSliderPage.java
    Added Javadoc.

--- src/main/java/pages/HorizontalSliderPage.java
+++ src/main/java/pages/HorizontalSliderPage.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 
+/**
+* This class models the horizontal slider page that is tested in the SliderTests test class (src/test/horizontalSlider). 
+*/ 
 public class HorizontalSliderPage {
 
     private WebDriver driver;
@@ -15,6 +18,7 @@ public class HorizontalSliderPage {
         this.driver=driver;
     }
 
+    // This method is called to move the slider until the desired value. Keys are sent while the message doesn't match the desired value.
     public void moveSlider(String value){
         while (!getSliderValue().equals(value)){
             driver.findElement(sliderInput).sendKeys(Keys.ARROW_RIGHT);
