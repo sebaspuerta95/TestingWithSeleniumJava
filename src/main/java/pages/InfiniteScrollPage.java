1ca154a139ecfc638df0169fbf6b7f8a50461384
Sebastian Puerta <133784886+sebaspuerta95@users.noreply.github.com>
2024-11-29 19:58:03 -0400

    Update InfiniteScrollPage.java
    Javadoc included.

--- src/main/java/pages/InfiniteScrollPage.java
+++ src/main/java/pages/InfiniteScrollPage.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
+/**
+* This class models the human scroll that is tested in the JavascriptTests test class (src/test/javascript). 
+*/
 public class InfiniteScrollPage {
 
     private WebDriver driver;
@@ -13,10 +16,14 @@ public class InfiniteScrollPage {
         this.driver = driver;
     }
 
+    /**
+    * The method sends human scrolls until the element is visible in the DOM.
+    * window.scrollTo() takes 2 arguments: X axis (it's 0 since we don't want horizontal slides), and Y axis (document.body.scrollHeight represents a human scroll).
+    * @param index : Represents the number of paragraphs that we want to show up.
+    */
     public void scrollToParagraph(int index){
         String script = "window.scrollTo(0, document.body.scrollHeight)";
-        var jsExecutor = ((JavascriptExecutor)driver);
-        // This is a different way of casting, but it does the same.
+        var jsExecutor = ((JavascriptExecutor)driver); // Polymorphism.
 
         while (getNumberOfParagraphsPresent() < index){
             jsExecutor.executeScript(script);
